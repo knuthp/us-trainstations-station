a5fad24660e4f017707785b1b292c222c6ea5f4b
Knut H Pedersen <knuthp@gmail.com>
2015-01-06 20:29:18 +0100

    Adjusted loggin

--- src/main/java/com/knuthp/microservices/trainstations/stations/RtStationListener.java
+++ src/main/java/com/knuthp/microservices/trainstations/stations/RtStationListener.java
@@ -1,15 +1,25 @@
 package com.knuthp.microservices.trainstations.stations;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.knuthp.microservices.trainstations.rt.domain.RtDepartures;
 import com.knuthp.microservices.trainstations.rt.domain.RtStop;
 
 public class RtStationListener {
+	private Logger logger = LoggerFactory.getLogger(RtStationListener.class);
 
 	public void update(RtDepartures rtDepartures) {
+		boolean isDelay = false;
 		for (RtStop rtStop : rtDepartures.getRtStopList()) {
 			if (rtStop.getDelay() != null && !"PT0S".equals(rtStop.getDelay())) {
-				System.out.println("Place: " + rtDepartures.getPlaceId() + ", id " + rtStop.getJourneyId() + ", line " + rtStop.getPublishedLineName() + " delayed " + rtStop.getDelay());
+				logger.info("Place: " + rtDepartures.getPlaceId() + ", id " + rtStop.getJourneyId() + ", line " + rtStop.getPublishedLineName() + " delayed " + rtStop.getDelay());
+				isDelay = true;
 			}
+		}
+		
+		if (!isDelay) {
+			logger.info("Place: " + rtDepartures.getPlaceId() + " no delays");
 		}
 		
 	}
